a78f8ada5d8b7e9735480568a63dd684e46a4a2e
jpe7s <jpe7s.salt188@passfwd.com>
2024-10-27 09:38:25 -0500

    fix visibility of dynamic drift markets.

--- programs/src/main/java/software/sava/anchor/programs/drift/DynamicPerpMarkets.java
+++ programs/src/main/java/software/sava/anchor/programs/drift/DynamicPerpMarkets.java
@@ -2,7 +2,7 @@ package software.sava.anchor.programs.drift;
 
 import java.net.http.HttpClient;
 
-record DynamicPerpMarkets(PerpMarkets mainNet, PerpMarkets devNet) {
+public record DynamicPerpMarkets(PerpMarkets mainNet, PerpMarkets devNet) {
 
   public static DynamicPerpMarkets fetchMarkets(final HttpClient httpClient) {
     return GenerateMarketConstants.createPerpMarkets(httpClient);
--- programs/src/main/java/software/sava/anchor/programs/drift/DynamicSpotMarkets.java
+++ programs/src/main/java/software/sava/anchor/programs/drift/DynamicSpotMarkets.java
@@ -2,7 +2,7 @@ package software.sava.anchor.programs.drift;
 
 import java.net.http.HttpClient;
 
-record DynamicSpotMarkets(SpotMarkets mainNet, SpotMarkets devNet) {
+public record DynamicSpotMarkets(SpotMarkets mainNet, SpotMarkets devNet) {
 
   public static DynamicSpotMarkets fetchMarkets(final HttpClient httpClient) {
     return GenerateMarketConstants.createSpotMarkets(httpClient);
