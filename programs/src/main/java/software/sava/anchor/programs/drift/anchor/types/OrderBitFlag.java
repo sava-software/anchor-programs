5695a071d8795982bb099fd99ec89d0baf4f8480
jpe7s <jpe7s.salt188@passfwd.com>
2025-04-11 12:37:03 -0500

    Sync Drift program.

---
+++ programs/src/main/java/software/sava/anchor/programs/drift/anchor/types/OrderBitFlag.java
@@ -0,0 +1,14 @@
+package software.sava.anchor.programs.drift.anchor.types;
+
+import software.sava.core.borsh.Borsh;
+
+public enum OrderBitFlag implements Borsh.Enum {
+
+  SignedMessage,
+  OracleTriggerMarket,
+  SafeTriggerOrder;
+
+  public static OrderBitFlag read(final byte[] _data, final int offset) {
+    return Borsh.read(OrderBitFlag.values(), _data, offset);
+  }
+}
